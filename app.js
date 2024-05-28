eb2822684e3cfe6b56bac1e469320942f1f2024a
Bas3L3ss <phamthen.hun060907@gmail.com>
2024-05-28 16:45:17 +0700

    commit

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log($(".card"));
+$(function () {
+  $(".card__container-information").slideUp();
+  $(".card__container-heading").click(function () {
+    // reset($(this));
+    $(this).next().slideToggle();
+    $(this).parent().toggleClass("active");
+  });
+});
+/* for only one is active */
+
+// const reset = (selectingElement) => {
+//   $(".khung").not(selectingElement.parent()).removeClass("active");
+//   $(".khung-info").not(selectingElement.next()).slideUp();
+// };
