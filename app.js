5729ea2d3eb7445cbbbbb105f4dff7e5b851405c
Bas3L3ss <phamthen.hun060907@gmail.com>
2024-05-28 17:09:53 +0700

    final commit

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
 $(function () {
   $(".card__container-information").slideUp();
   $(".card__container-heading").click(function () {
-    // reset($(this));
     $(this).next().slideToggle();
     $(this).parent().toggleClass("active");
   });
 });
-/* for only one is active */
-
-// const reset = (selectingElement) => {
-//   $(".khung").not(selectingElement.parent()).removeClass("active");
-//   $(".khung-info").not(selectingElement.next()).slideUp();
-// };
